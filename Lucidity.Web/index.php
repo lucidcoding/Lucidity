99b5ee1295432ab4adba2fa6916c8e42cf4e794d
Paul Davies <pd_account_2@hotmail.com>
2014-11-02 19:28:52 +0000

    Initial Commit

---
+++ Lucidity.Web/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Lucidity Software</title>
+    <link href="Styles/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="Styles/site.css" rel="stylesheet" type="text/css" />
+   <!--  <style>
+      body{
+        background-color: #636263;
+      }
+
+   img {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        margin-top: -220px;
+        margin-left: -218px;
+      }
+
+      div.links {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        width: 200px;
+        margin-top: 180px;
+        margin-left: -100px;
+        text-align: center;
+      }
+
+      a {
+        font-family: arial;
+        font-size: 9pt;
+        color: #faef2d;
+      }
+
+    </style> -->
+  </head>
+  <body>
+    <div class="container header" >
+        <img src="Images/Lucidity.gif" alt="Lucidity Software"/>
+    </div>
+<!--     <div class="links" >
+        <a href="mailto:info@luciditysoftware.co.uk">info@luciditysoftware.co.uk</a>
+        <br/>
+        <a href="http://www.lucidcoding.blogspot.co.uk">www.lucidcoding.blogspot.co.uk</a>
+    </div> -->
+  </body>
+</html>
+
+
+<?php
+
+echo "Hello World!";
+
+?>
