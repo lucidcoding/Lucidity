9fb7f4716d7e996b757cfca63d58f409dc6a29da
Paul Davies <pd_account_2@hotmail.com>
2014-11-19 18:28:54 +0000

    Bootstrap updates

--- Lucidity.Web/index.php
+++ Lucidity.Web/index.php
@@ -1,52 +1,56 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <title>Lucidity Software</title>
     <link href="Styles/bootstrap.css" rel="stylesheet" type="text/css" />
     <link href="Styles/site.css" rel="stylesheet" type="text/css" />
-   <!--  <style>
-      body{
-        background-color: #636263;
-      }
-
-   img {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        margin-top: -220px;
-        margin-left: -218px;
-      }
-
-      div.links {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        width: 200px;
-        margin-top: 180px;
-        margin-left: -100px;
-        text-align: center;
-      }
-
-      a {
-        font-family: arial;
-        font-size: 9pt;
-        color: #faef2d;
-      }
-
-    </style> -->
+    <script src="Scripts/jquery-1.9.1.js" type="text/javascript"></script>
+    <script src="Scripts/bootstrap.js" type="text/javascript"></script>
+    
   </head>
-  <body>
-    <div class="container-fluid header" >
-        <img src="Images/Lucidity.gif" alt="Lucidity Software"/>
-    </div>
-    <div class="container-fluid" >
-    </div>
-<!--     <div class="links" >
-        <a href="mailto:info@luciditysoftware.co.uk">info@luciditysoftware.co.uk</a>
-        <br/>
-        <a href="http://www.lucidcoding.blogspot.co.uk">www.lucidcoding.blogspot.co.uk</a>
-    </div> -->
-  </body>
+    <body>
+    
+      
+      
+        <header role="banner">
+            <img id="logo-main" src="Images/Lucidity.gif" width="300" alt="Lucidity Software"/>
+            <!-- <nav id="navbar-primary" class="navbar" role="navigation"> -->
+            <nav id="navbar" class="navbar navbar-default" role="navigation" >
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                    
+      
+            
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-primary-collapse" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Please select:</a>
+                    </div>
+       
+                    <div class="collapse navbar-collapse" id="navbar-primary-collapse" ">
+                        <ul class="nav navbar-nav">
+                          <li class="active"><a href="#">Link</a></li>
+                          <li><a href="#">Link</a></li>
+                          <li><a href="#">Link</a></li>
+                          <li><a href="#">Link</a></li>
+                          <li><a href="#">Link</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                
+            
+            
+                </div><!-- /.container-fluid -->
+            </nav>
+        </header><!-- header role="banner" -->
+
+    </body>
+  
 </html>
 
 
