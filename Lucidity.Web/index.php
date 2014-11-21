fd8662e3085f0788f1fc94d13a25625bdc808e27
Paul Davies <pd_account_2@hotmail.com>
2014-11-21 20:06:16 +0000

    Design changes

--- Lucidity.Web/index.php
+++ Lucidity.Web/index.php
@@ -11,8 +11,9 @@
       <?php include('header.php'); ?>
 
       <main role="main" class="container-fluid">
-        <article class="row">
-            <section class="col-sm-12 col-md-12 col-lg-12">
+        <div class=row">
+        <article class="col-sm-12 col-md-9 col-lg-9">
+            <section >
                 <h1>
                     Bespoke software development services
                 </h1>
@@ -26,6 +27,17 @@
                 </p>
             </section>
         </article>
+        <aside class="col-sm-12 col-md-3 col-lg-3 contact-aside">
+            <h1>Contact Me</h1>
+            <a href="http://lucidcoding.blogspot.com"><img src="Images/glyphicons_social_05_blogger.png" alt="blogger" /></a>
+            <a href="http://www.linkedin.com/company/lucidity-software"><img src="Images/glyphicons_social_17_linked_in.png" alt="blogger" /></a>
+            <a href="http://www.facebook.com/luciditysoftware"><img src="Images/glyphicons_social_30_facebook.png" alt="blogger" /></a>
+            <a href="#"><img src="Images/glyphicons_social_31_twitter.png" alt="blogger" /></a>
+            <span class="glyphicon icon-facebook"></span>
+            <span class="glyphicon icon-twitter"></span>
+            Email: info@luciditysoftware.co.uk
+        </aside>
+        </div>
       </main>
     </body>
   
